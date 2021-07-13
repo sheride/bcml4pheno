1463489a1621bdf3ba8024c491e88178d5e914fc
Elijah Sheridan <elijah.sheridan@vanderbilt.edu>
2021-07-13 18:42:45 -0400

    many new library files, working on a full example

--- bcml4pheno/_nbdev.py
+++ bcml4pheno/_nbdev.py
@@ -3,15 +3,20 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"bcml_model": "bcml_model.ipynb",
-         "plt_settings": "data_vis.ipynb",
+         "refresh_model": "bcml_model.ipynb",
+         "label_predictors": "data_preproc.ipynb",
+         "get_settings": "data_vis.ipynb",
          "cross_section_helper": "data_vis.ipynb",
+         "multi_model": "multi_model.ipynb",
          "get_elijah_ttbarzp_cs": "ttbarzp.ipynb",
          "get_manuel_ttbarzp_cs": "ttbarzp.ipynb",
          "import47Ddata": "ttbarzp.ipynb",
          "get47Dfeatures": "ttbarzp.ipynb"}
 
 modules = ["bcml_model.py",
+           "data_preproc.py",
            "data_vis.py",
+           "multi_model.py",
            "ttbarzp.py"]
 
 doc_url = "https://sheride.github.io/bcml4pheno/"
